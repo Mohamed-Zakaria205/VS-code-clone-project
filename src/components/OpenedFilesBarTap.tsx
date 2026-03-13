c21e38514f299536e6b6fd95877d3be2b78f54e2
Mohamed Zakaria <anamohamedda977@gmail.com>
2026-03-13 06:22:59 +0200

    Remove tap handler

--- src/components/OpenedFilesBarTap.tsx
+++ src/components/OpenedFilesBarTap.tsx
@@ -2,7 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import type { IFile } from "../interfaces";
 import RenderFileIcon from "./RenderFileIcon";
 import CloseIcon from "./SVG/CloseIcon";
-import { setClickedFileAction } from "../app/features/fileTreeSlice";
+import {
+  setClickedFileAction,
+  setOpenedFilesAction,
+} from "../app/features/fileTreeSlice";
 import type { RootState } from "../app/store";
 
 interface IProps {
@@ -13,6 +16,7 @@ const OpenedFilesBarTap = ({ file }: IProps) => {
   const dispatch = useDispatch();
   const {
     clickedFile: { activeTapID },
+    openedFiles,
   } = useSelector((state: RootState) => state.tree);
   const onClick = () => {
     dispatch(
@@ -23,6 +27,32 @@ const OpenedFilesBarTap = ({ file }: IProps) => {
       }),
     );
   };
+
+  const onRemove = (e: React.MouseEvent<HTMLSpanElement>, id: string) => {
+    e.stopPropagation();
+    const filtered = openedFiles.filter((file) => file.id !== id);
+    const lastTap = filtered[filtered.length - 1];
+
+    if (!lastTap) {
+      dispatch(
+        setClickedFileAction({
+          fileName: "",
+          fileContent: "",
+          activeTapID: null,
+        }),
+      );
+      dispatch(setOpenedFilesAction([]));
+      return;
+    }
+    dispatch(setOpenedFilesAction(filtered));
+    dispatch(
+      setClickedFileAction({
+        fileName: lastTap?.name,
+        fileContent: lastTap?.content,
+        activeTapID: lastTap.id,
+      }),
+    );
+  };
   return (
     <div
       className={`flex items-center p-2 border-t-2 ${file.id === activeTapID ? " border-[#cf6ccf]" : "border-transparent"}  `}
@@ -30,7 +60,10 @@ const OpenedFilesBarTap = ({ file }: IProps) => {
     >
       <RenderFileIcon fileName={file.name} />
       <span className="cursor-pointer mx-1 p-1"> {file.name}</span>
-      <span className="cursor-pointer hover:bg-gray-600 duration-300 rounded-md w-fit mr-2 p-1">
+      <span
+        className="cursor-pointer hover:bg-gray-600 duration-300 rounded-md w-fit mr-2 p-1"
+        onClick={(e) => onRemove(e, file.id)}
+      >
         <CloseIcon />
       </span>
     </div>
